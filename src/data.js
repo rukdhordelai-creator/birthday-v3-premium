5e79b22aea72b31e2be81de00197427f117946bd
rukdhordelai-creator <rukdhordelai@gmail.com>
2025-12-25 21:42:20 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,61 @@
+// ==========================
+// 1. Intro Screen
+// ==========================
+export const firstHeading = "A Cutiepie was born today, 20 years ago!"
+// Main heading text - Change this text as you like
+
+export const firstSubtext = "Yes, it’s YOU! A little surprise awaits..."
+// Subtext under heading (optional) - Change, leave blank or remove.
+
+
+// ==========================
+// 2. Cake Screen
+// ==========================
+// Add recipient's name here.
+export const NAME = "Joshnaa"
+
+
+// ==========================
+// 3. Photo Screen
+// ==========================
+export const photoScreenHeading = "Some Sweet Moments"
+// Heading above photos - Change this text as you like
+
+// Add your photos inside "public/images" folder
+// Example: { "/images/1.jpeg" }, Match the format and add more as needed
+export const photos = [
+    "/images/1.jpeg",
+    "/images/2.jpeg",
+    "/images/3.jpeg",
+    "/images/4.jpeg",
+    "/images/5.jpeg",
+    
+]
+
+
+// ==========================
+// 4. Message Screen
+// ==========================
+export const messageScreenHeading = "A Special Message"
+// Heading above card - Change this text as you like
+
+export const specialMessage = `Happy Birthday, Cutiepie! You deserve all the happiness, love, and smiles in the world today and always. You have this special way of making everything around you brighter, your smile, your kindness, and the way you make people feel truly cared for. I hope your day is filled with laughter, surprises, and moments that make your heart happy. You’re truly one of a kind, and I just want you to know how special you are. Keep being the amazing person you are, spreading joy wherever you go. Wishing you endless happiness, success, and all the sweet things life has to offer. 💗`
+// Main special message (you can edit freely)
+
+
+// ==========================
+// 4. Final Screen
+// ==========================
+export const finalScreenHeading = "One Last Thing..."
+// Heading above final surprise - Change this text as you like
+
+export const overlayText = "Lots of love for you ❤️"
+// First text inside the overlay card - Change this text as you like
+
+export const overlayMessage = "Once again, Happy Birthday! Hope you loved your surprise."
+
+// ==========================
+// Background Music
+// ==========================
+// Place your .mp3 file inside "public/audio" and update path below
+export const backgroundMusic = "./audio/bg.mp3"
